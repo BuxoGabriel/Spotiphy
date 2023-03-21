b4f32cfaa500e03955b4b52e67608d41fd366751
Gabriel <gabrielbuxo1@gmail.com>
2023-03-21 11:30:05 -0400

    Added User input tokenizer and basic ux

--- CLI.py
+++ CLI.py
@@ -1,5 +1,6 @@
 import psycopg2
 from sshtunnel import SSHTunnelForwarder
+import Commands
 
 def loadConfig():
     with open(".env", "r") as f:
@@ -22,15 +23,30 @@ def main():
                 'host': 'localhost',
                 'port': server.local_bind_port
             }
+            
+            print("Welcome to Spotiphy! The Commands that can be used are as follows")
+            Commands.Help()
+            while True:
+                userInp = input().split()
+                args = len(userInp)
+                command = userInp[0]
+                if args == 0:
+                    continue
+                match command:
+                    case "quit":
+                        break
+                    case "help":
+                        Commands.Help()
 
             conn = psycopg2.connect(**params)
             curs = conn.cursor()
             print("Database connection established")
             print(curs.fetchall())
-            conn.close()
     except Exception as e:
         print("Connection failed")
         print(e)
+    finally:
+        conn.close()
 
 if __name__ == "__main__":
     main()
